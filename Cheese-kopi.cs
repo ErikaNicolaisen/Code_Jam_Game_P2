33b018cc9151ec5ef8436359b0a4b74ac5c7c477
ErikaNicolaisen <147511548+ErikaNicolaisen@users.noreply.github.com>
2024-05-27 09:14:20 +0200

    Add files via upload

--- Cheese-kopi.cs
+++ Cheese-kopi.cs
@@ -21,6 +21,11 @@ public class Cheese : MonoBehaviour
         }
     }
 
+    //this script should also have code for spawning the different cheeses
+    //which can be overriden in the two childclasses, but for now, this is
+    //implemented in the CelestialBodySpawner. For now the child and parent
+    //classes only consists of the behavior of the different cheeses.
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(collision.gameObject);
